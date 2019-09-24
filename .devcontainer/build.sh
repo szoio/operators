483f766122afff60bf8533df00fc6fad58466b46
Stephen Zoio <stephen.zoio@team.telstra.com>
2019-09-24 16:27:01 +1000

    more changes

--- .devcontainer/build.sh
+++ .devcontainer/build.sh
@@ -1,4 +1,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 VERSION=0.1.0
 
-docker build -t operator-dev-container:$VERSION -f $DIR/Dockerfile $DIR/..
+docker build -t operator-dev-container:$VERSION -f $DIR/Dockerfile $DIR/.. $1
